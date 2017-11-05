1f30cd90fd179cded77c5b74d6afe39cc4efc24a
PATIPAN NAKARIN <Kokoskun@gmail.com>
2017-11-05 13:41:55 +0700

    Chatbot Facebook

---
+++ bot.php
@@ -0,0 +1,28 @@
+<?php
+$hubVerifyToken = '';
+$accessToken =   "";
+if ($_REQUEST['hub_verify_token'] === $hubVerifyToken) {
+  echo $_REQUEST['hub_challenge'];
+  exit;
+}
+$input = json_decode(file_get_contents('php://input'), true);
+$senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
+$messageText = $input['entry'][0]['messaging'][0]['message']['text'];
+$response = null;
+if(strpos($json_output,'hi')!==FALSE) {
+    $answer = "Hello";
+}else{
+	$answer = "No";
+}
+$response = [
+    'recipient' => [ 'id' => $senderId ],
+    'message' => [ 'text' => $answer ]
+];
+$ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
+curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+if(!empty($input)){
+$result = curl_exec($ch);
+}
+curl_close($ch);
